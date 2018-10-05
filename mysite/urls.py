a7fe154eec31d0e0e4bee91a9a3f52990f4b8ab7
zulakawomela <zulanats@gmail.com>
2018-10-06 00:29:04 +0300

    Add views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+url(r'^admin/', admin.site.urls),
+url(r'^accounts/login/$', views.login, name='login'),
+url(r'', include('blog.urls')),
+url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+url(r'', include('blog.urls')),
 ]
